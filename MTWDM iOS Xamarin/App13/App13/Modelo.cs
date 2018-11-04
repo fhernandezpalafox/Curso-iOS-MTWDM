fc50fef63bdc1c9ae00a3efc41d403d1baab7ddb
Felipe Hernandez Palafox <felipe.sistemas.fhp@gmail.com>
2018-11-03 18:38:20 -0600

    Curso de iOS
    Curso de iOS

---
+++ MTWDM iOS Xamarin/App13/App13/Modelo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App13
+{
+    public class Modelo
+    {
+        public Dictionary<string, List<string>> lugares =
+            new Dictionary<string, List<string>>();
+
+        public Modelo() {
+            lugares.Add("A", new List<string> { "Angkor Wat, Cambodia", "Aogashima Volcano, Japan" });
+            lugares.Add("B", new List<string> { "Bamboo Forest, Japan", "Big Island, Hawaii, USA", "Bora Bora, French Polynesia" });
+            lugares.Add("C", new List<string> { "Chittorgarh Fort, India", "Cinque Terre, Rio Maggiore, Italy" });
+            lugares.Add("F", new List<string> { "Fairy Pools, Isle of Skye, Scotland", "Fiji-Japan", "Fingal’s Cave, Scotland" });
+            lugares.Add("G", new List<string> { "Galapagos, Ecuador ", "Giant’s Causeway, Ireland", "Giza Pyramids, Egypt", "Glass Beach, California, USA", "Glow worm cave, New Zealand", "Great Blue Hole of Belize" });
+            lugares.Add("H", new List<string> { "Ha Long Bay, Vietnam", "Hiller lake(pink lake), Western Australia", "Horsetail Falls, Yosemite National Park", "Hvitserkur, North Iceland" });
+            lugares.Add("M", new List<string> { "Machu Picchu, Peru", "Mamanuca Islands, Fiji" });
+            lugares.Add("N", new List<string> { "Neuschwanstein Castle, Germany" });
+            lugares.Add("P", new List<string> { "Pamukkale, Turkey", "Panjin Red Beach, China", "Petra, Jordan", "Puma Punku, Bolivia" });
+            lugares.Add("R", new List<string> { "Rice Terrace Fields in Mu Cang Chai, Vietnam" });
+            lugares.Add("S", new List<string> { "Santorini, Greece", "Sea Cliffs, Etretat, France", "St. Lucia" });
+            lugares.Add("T", new List<string> { "Tianzi Mountains, China" });
+            lugares.Add("V", new List<string> { "Venice, Italy" });
+            lugares.Add("Z", new List<string> { "Zhangye Danxia Landform in Gansu, China", "Zion National Park, Utah, USA" });
+            lugares.Add("Y", new List<string> { "Yellowstone National Park, Wyoming, USA", "Yosemite Valley, USA" });
+
+
+        }
+    }
+}
---
+++ MTWDM iOS Xamarin/App13/App13/ViewController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using UIKit;
+using System.Linq;
+using System.Collections.Generic;
+using Foundation;
+
+namespace App13
+{
+    public partial class ViewController : UITableViewController
+    {
+        Modelo modelo;
+        List<string> alfabeto;
+        string[] alfabetoarray;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            modelo = new Modelo();
+
+            alfabeto = modelo.lugares.Keys.ToList();
+            alfabeto.Sort();
+            alfabetoarray = alfabeto.ToArray();
+            Console.WriteLine(alfabetoarray);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+       
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return modelo.lugares.Count;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            var letra = alfabetoarray[section];
+
+            var countLetras = alfabetoarray.Count();
+
+
+            switch ((int)section)
+            {
+                case int n when (n<=countLetras):
+                    return modelo.lugares[letra].Count();
+                default:
+                    return 1;
+            }
+
+
+            /*
+             //SWIFT
+                 let letra = alfabetoArreglo[section]
+        
+                    switch section {
+                    case 0...alfabetoArreglo.count: return modelo.lugares[letra]!.count
+                        //case 0...14: return modelo.lugares[letra]!.count
+                    default: return 1
+                    }
+            */
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+
+            var cell = tableView.DequeueReusableCell("lugaresCell", indexPath);
+
+            var letra = alfabetoarray[indexPath.Section];
+
+            var countLetras = alfabetoarray.Count();
+
+            switch (indexPath.Section)
+            {
+                case int n when (n <= countLetras):
+                    var contenido = modelo.lugares[letra];
+                    var row = contenido[indexPath.Row];
+                    cell.TextLabel.Text = row;
+                    break;
+                default: break;
+            }
+
+            return cell;
+
+
+            /*
+             //SWIFT
+
+                       let cell = tableView.dequeueReusableCell(withIdentifier: "lugaresCell", for: indexPath)
+        
+                        let letra = alfabetoArreglo[indexPath.section]
+                        
+                        switch indexPath.section {
+                            
+                        //case 0...14: cell.textLabel?.text = modelo.lugares[letra]?[indexPath.row]
+                        case 0...alfabetoArreglo.count: cell.textLabel?.text = modelo.lugares[letra]?[indexPath.row]
+                        default: break
+                        }
+                        
+                        return cell
+            */
+
+        }
+
+        public override string TitleForHeader(UITableView tableView, nint section)
+        {
+            var letra = alfabetoarray[section];
+
+            var countLetras = alfabetoarray.Count();
+
+            switch ((int)section)
+            {
+                case int n when (n <= countLetras):
+                    return letra;
+                default: return letra; 
+            }
+        }
+    }
+}
