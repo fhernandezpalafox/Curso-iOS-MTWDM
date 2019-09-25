0235f785dfa3304c0afc2fa091a359ced33cf892
Felipe Hernandez <felipe.sistemas.fhp@gmail.com>
2019-09-25 08:32:06 -0500

    Curso de iOS
    Curso de iOS

---
+++ MTWDM iOS Xamarin/AppChangeTheme/AppChangeTheme/ViewController.cs
@@ -0,0 +1,58 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace AppChangeTheme
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+       
+            if (TraitCollection.UserInterfaceStyle  == UIUserInterfaceStyle.Light)
+            {
+                Console.WriteLine("Modo Luz");
+            }
+            else
+            {
+                Console.WriteLine("Modo Oscuro");
+            }
+
+            this.OverrideUserInterfaceStyle = UIUserInterfaceStyle.Dark;
+
+
+        }
+
+        partial void onCambiarTema(Foundation.NSObject sender)
+        {
+            var segmented = sender as UISegmentedControl;
+
+
+            switch (segmented.SelectedSegment)
+            {
+                case 0:
+                    this.OverrideUserInterfaceStyle = UIUserInterfaceStyle.Dark;
+                    break;
+                case 1:
+                    this.OverrideUserInterfaceStyle = UIUserInterfaceStyle.Light;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ MTWDM iOS Xamarin/AppChangeTheme/AppChangeTheme/ViewController.designer.cs
@@ -0,0 +1,22 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace AppChangeTheme
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("onCambiarTema:")]
+		partial void onCambiarTema (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
